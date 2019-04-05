d2ddb05b1b219358e0501d1ac25028a49aad2e11
matheus-bernardino <matheusarj97@gmail.com>
2019-04-05 16:36:20 -0300

    intial commit

---
+++ src/AST/Or.java
@@ -0,0 +1,18 @@
+package AST;
+
+import AST.Visitor.Visitor;
+import java_cup.runtime.ComplexSymbolFactory.Location;
+
+public class Or extends Exp{
+	public Exp e1, e2;
+	
+	public Or(Exp ae1, Exp ae2, Location pos) {
+		super(pos);
+		e1=ae1; e2=ae2;
+	}
+
+	@Override
+	public void accept(Visitor v) {
+		v.visit(this);
+	}
+}
