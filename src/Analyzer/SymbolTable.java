0d0f61b319d5b3879ad55979d011193a6f03cb94
matheus-bernardino <matheusarj97@gmail.com>
2019-05-08 01:17:51 -0300

    add methods to symbol table

--- src/Analyzer/SymbolTable.java
+++ src/Analyzer/SymbolTable.java
@@ -17,6 +17,75 @@ public class SymbolTable {
 		this.table = table;
 	}
 	
+	public void setClass(ClassAnalyzer _class) {
+		if(table.contains(_class.getClassName())) 
+			table.put(_class.getClassName(), _class);
+	}
 	
+	public ClassAnalyzer getClass(String _className) {
+		if(table.contains(_className)) 
+			return table.get(_className);
+		
+		return null;
+	}
 
+	public boolean checkVariable(ClassAnalyzer _class, MethodAnalyzer _method, String variableName) {
+		if(!(table.containsKey(_class.getClassName()))) 
+			return false;
+
+		ClassAnalyzer auxClass = getClass(_class.getClassName());
+		if (auxClass.getClassVariables().containsKey(variableName))
+			return true;
+		
+		if (table.containsKey(_class.getClassName()) && _class.getClassMethods().containsKey(_method.getName())){
+			if (_class.getClassMethods().containsKey(_method.getName())){
+				MethodAnalyzer auxMethod = _class.getClassMethods().get(_method.getName());
+				
+				if(auxMethod.getArguments().containsKey(variableName))
+					return true;
+				else if(auxMethod.getLoacalVariables().containsKey(variableName))
+					return true;
+			}
+		}
+
+		auxClass = getClass(_class.getParentClassName());
+		while (auxClass != null) {
+			if (auxClass.getClassVariables().containsKey(variableName))
+				return true;
+
+			auxClass = getClass(auxClass.getParentClassName());
+		}
+		
+		return false;
+	}
+	
+	public VariableAnalyzer getVariable(ClassAnalyzer _class, MethodAnalyzer _method, String variableName) {
+		if(!(table.containsKey(_class.getClassName()))) 
+			return null;
+
+		ClassAnalyzer auxClass = getClass(_class.getClassName());
+		if (auxClass.getClassVariables().containsKey(variableName))
+			return auxClass.getClassVariables().get(variableName);
+		
+		if (table.containsKey(_class.getClassName()) && _class.getClassMethods().containsKey(_method.getName())){
+			if (_class.getClassMethods().containsKey(_method.getName())){
+				MethodAnalyzer auxMethod = _class.getClassMethods().get(_method.getName());
+				
+				if(auxMethod.getArguments().containsKey(variableName))
+					return auxMethod.getArguments().get(variableName);
+				else if(auxMethod.getLoacalVariables().containsKey(variableName))
+					return auxMethod.getLoacalVariables().get(variableName);
+			}
+		}
+
+		auxClass = getClass(_class.getParentClassName());
+		while (auxClass != null) {
+			if (auxClass.getClassVariables().containsKey(variableName))
+				return auxClass.getClassVariables().get(variableName);
+
+			auxClass = getClass(auxClass.getParentClassName());
+		}
+		
+		return null;
+	}
 }
